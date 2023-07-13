6a0480cbb172a87afd590d58691e0cc8091fd153
Ranjith <ranjithstar256@gmail.com>
2023-07-13 14:52:38 +0530

    added printing log

--- app/src/main/java/com/example/myworkmanagerexpl/MainActivity.kt
+++ app/src/main/java/com/example/myworkmanagerexpl/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.myworkmanagerexpl
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -13,26 +11,28 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.app.NotificationCompat
+import androidx.lifecycle.Observer
 import androidx.work.Constraints
 import androidx.work.Data
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import com.example.myworkmanagerexpl.ui.theme.MyWorkManagerExplTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var workManager: WorkManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             MyWorkManagerExplTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -42,16 +42,24 @@ class MainActivity : ComponentActivity() {
                     // what is the work ? = show notification
                     // what is the condition = constrains
                     // periodic or onetime? = onetime
+                    workManager = WorkManager.getInstance(applicationContext)
 
-                       val powerConstraint = Constraints.Builder().setRequiresCharging(true).build()
-                    // val taskData = Data.Builder().putString("MESSAGE_STATUS", "Notify Done.").build()
+                       val powerConstraint = Constraints.Builder().setRequiredNetworkType(networkType = NetworkType.NOT_ROAMING).setRequiresCharging(true).build()
+                   // val taskData = Data.Builder().putString("MESSAGE_STATUS", "Notify Done.").build()
 
 
+                    //PeriodicWorkRequest
                     val request = OneTimeWorkRequest.Builder(MyWorkCls::class.java).setConstraints(powerConstraint).build()
 
                     var bt by remember { mutableStateOf(false) }
+
                     if (bt) {
                         WorkManager.getInstance(applicationContext).enqueue(request)
+
+                        workManager.getWorkInfoByIdLiveData(request.id)
+                            .observe(this) {
+                                    println("========== Work status: $it.status  \n")
+                            }
                     }
                     Column() {
                         Button(onClick = {
